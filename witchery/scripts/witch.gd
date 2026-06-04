533c81477858fd3baca71fcab6d8ea2cdc8c03f7
Khank <knguyen3@students.mags.school.nz>
2026-06-05 09:38:46 +1200

    character movement

---
+++ witchery/scripts/witch.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+var speed: int = 300
+var health: int = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction: Vector2 = Vector2(0.0,0.0)
+	direction.x = Input.get_axis("ui_left", "ui_right")
+	direction.y = Input.get_axis("ui_up", "ui_down")
+	velocity = speed * direction.normalized()
+	move_and_slide()
