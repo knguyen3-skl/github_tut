d8d61f495e0146510f269c2e8241949f6251a7cc
Khank <knguyen3@students.mags.school.nz>
2026-06-17 21:28:22 +1200

    broken respawn timer

--- witchery/scenes/main_level.gd
+++ witchery/scenes/main_level.gd
@@ -18,8 +18,20 @@ func _process(delta: float) -> void:
 	# if alive show
 	# if dead hide + untouchable
 	for enemies in get_tree().get_nodes_in_group("enemy"):
-		if Global.enemy_dict[enemies.name] == "dead":
+		if Global.enemy_dict[enemies.name] == "dead" and potato_respawn.is_stopped():
 			enemies.hide()
 			enemies.get_child(2).monitoring = false
 			enemies.get_child(1).disabled = true
 			enemies.set_physics_process(false)
+			potato_respawn.start()
+			
+		elif Global.enemy_dict[enemies.name] == "alive":
+			enemies.show()
+			enemies.get_child(2).monitoring = true
+			enemies.get_child(1).disabled = false
+			enemies.set_physics_process(true)
+
+func _respawn_enemy() -> void:
+	Global.enemy_dict[Global.enemy_id] = "alive"
+	print(Global.enemy_dict)
+	potato_respawn.stop()
